72ac9f1f7a8809d1438a157497d560a73a0f70f9
Rafael Menchaca <arafael.menchaca@gmail.com>
2026-04-26 19:36:29 -0500

    feat lista de cotejos: se crea lista de cotejo en base a la actividad de cierre

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import planeacionesRoutes from './planeaciones.routes.js';
 import examenesRoutes from './examenes.routes.js';
 import jerarquiaRoutes from './jerarquia.routes.js';
+import listasCotejoRoutes from './listas_cotejo.routes.js';
 
 const router = Router();
 
 router.use('/planeaciones', planeacionesRoutes);
 router.use('/examenes', examenesRoutes);
+router.use('/listas-cotejo', listasCotejoRoutes);
 router.use('/', jerarquiaRoutes);
 
 export default router;
